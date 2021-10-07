motioneye-project/motioneyeos

commit c212a90b612adb18aba7d74c74bda863e372d265

package/dfu-util: bump version to 0.8, add hash, fix SITE url
http://dfu-util.gnumonks.org is down

Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

package/dfu-util/dfu-util.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-DFU_UTIL_VERSION = 0.6
-DFU_UTIL_SITE = http://dfu-util.gnumonks.org/releases
+DFU_UTIL_VERSION = 0.8
+DFU_UTIL_SITE = http://dfu-util.sourceforge.net/releases
 DFU_UTIL_LICENSE = GPLv2+
 DFU_UTIL_LICENSE_FILES = COPYING
 

package/dfu-util/dfu-util.hash
@@ -1,0 +1,2 @@
+# From http://dfu-util.sourceforge.net/releases/dfu-util-0.8.tar.gz.md5
+md5	fc6daf6b0ee57d7e40ffa3e8111023d1	dfu-util-0.8.tar.gz

LeSpocky/buildroot

commit 332e9c9a9b88473b10c362101b22e1ca7486a6bd

package/dfu-util: Fix licenses
Since v0.8 a script 'dfuse-pack.py' is part of the package, which has a
different license.

Fixes: c212a90b612a ("package/dfu-util: bump version to 0.8, add hash, fix SITE url")
Signed-off-by: Alexander Dahl <post@lespocky.de>

package/dfu-util/dfu-util.mk
@@ -6,7 +6,7 @@
 
 DFU_UTIL_VERSION = 0.10
 DFU_UTIL_SITE = http://dfu-util.sourceforge.net/releases
-DFU_UTIL_LICENSE = GPL-2.0+
+DFU_UTIL_LICENSE = GPL-2.0+, LGPL-3.0
 DFU_UTIL_LICENSE_FILES = COPYING
 
 DFU_UTIL_DEPENDENCIES = libusb